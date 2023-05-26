64480985db1bbfd5d10a0c4fcb26d1692a0f2203
John Landers <jclanders@protonmail.com>
2023-05-25 21:13:27 -0500

    contact form sizing

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -11,29 +11,33 @@ export default function Contact() {
     }
 
     return <form className="container" onSubmit={handleSubmit(onSubmit)}>
-        <div className="mb-3">
-            <label htmlFor="from" className="form-label">Email address</label>
-            <input  id="from"
-                    name="from" 
-                    className="form-control"
-                    {...register("from", {autoComplete: "email", type: "email"})}/>
+        <div className="row justify-content-center">
+            <div className="col-xxl-6 col-sm-8">
+                <div className="mb-3">
+                    <label htmlFor="from" className="form-label">Email address</label>
+                    <input  id="from"
+                            name="from" 
+                            className="form-control"
+                            {...register("from", {autoComplete: "email", type: "email"})}/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="subject" className="form-label">Subject</label>
+                    <input  type="text"
+                            id="subject"
+                            name="subject" 
+                            className="form-control"
+                            {...register("subject", {required: true, type: "text"})}/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="message" className="form-label">Message</label>
+                    <textarea   id="message" 
+                                name="message" 
+                                className="form-control" 
+                                rows={10}
+                                {...register("message", {required: true})}/>
+                </div>
+                <button name="submitButton" className="btn btn-primary bg-light text-dark border-dark w-100" type="submit">Send</button>
+            </div>
         </div>
-        <div className="mb-3">
-            <label htmlFor="subject" className="form-label">Subject</label>
-            <input  type="text"
-                    id="subject"
-                    name="subject" 
-                    className="form-control"
-                    {...register("subject", {required: true, type: "text"})}/>
-        </div>
-        <div className="mb-3">
-            <label htmlFor="message" className="form-label">Message</label>
-            <textarea   id="message" 
-                        name="message" 
-                        className="form-control" 
-                        rows={10}
-                        {...register("message", {required: true})}/>
-        </div>
-        <button name="submitButton" className="btn btn-primary bg-light text-dark border-dark w-100" type="submit">Send</button>
     </form>;
 }
